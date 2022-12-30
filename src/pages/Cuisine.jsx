b9062e3178b18f5e69dfc3d5e6fd10e346b05d75
pagoli <oliviapagel89@gmail.com>
2022-12-30 11:51:54 +0100

    data backup added in case of no free requests

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,10 +1,11 @@
-import React, { useContext, useEffect } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { Context } from "../context/context";
 import { Wrapper, Grid, Card } from "../styles/CuisineStyle";
 import { Link, useParams } from "react-router-dom";
 // useParams allows to pull out the keyword from the link
 
 const Cuisine = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const { cuisine, setCuisine } = useContext(Context);
 
   let params = useParams();
@@ -16,12 +17,16 @@ const Cuisine = () => {
         import.meta.env.VITE_API_KEY
       }&number=9&${searchQuery}`
     );
+
+    if (data.status !== 200) {
+      const localData = await fetch(`../../src/data/${name}.json`);
+      const recipes = await localData.json();
+
+      setCuisine(recipes);
+    }
     const recipes = await data.json();
+
     setCuisine(recipes.results);
-    // localStorage.setItem(
-    //   `${name.toLowerCase()}`,
-    //   JSON.stringify(recipes.results)
-    // );
   };
 
   const getQuery = (name) => {
@@ -45,8 +50,34 @@ const Cuisine = () => {
   };
 
   useEffect(() => {
-    getCuisine(params.type);
+    if (cuisine !== undefined || cuisine !== null) {
+      setIsLoading(false);
+    }
+    if (params.type !== undefined) {
+      const cuisine = JSON.parse(
+        localStorage.getItem(`${params.type.toLowerCase()}`)
+      );
+      console.log("cuisine Local =>", cuisine);
+      setCuisine(cuisine);
+    } else {
+      getCuisine(params.type);
+    }
   }, [params.type]);
+
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        delayChildren: 0.5,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1 },
+  };
 
   return (
     <Wrapper
@@ -60,15 +91,16 @@ const Cuisine = () => {
       ) : (
         <></>
       )}
-      <Grid>
-        {cuisine === undefined || cuisine === null ? (
+      <Grid variants={container} initial="hidden" animate="show">
+        {console.log("CUISINE=> ", cuisine)}
+        {(isLoading && cuisine == undefined) || cuisine === null ? (
           <div>
             <h3>No recipes</h3>
           </div>
         ) : (
           cuisine.map((recipe) => {
             return (
-              <Card key={recipe.id}>
+              <Card key={recipe.id} variants={item}>
                 <Link to={`/recipe/${recipe.id}`}>
                   <img src={recipe.image} alt={recipe.title} />
                   <h4>{recipe.title}</h4>
