1799ec31378cab0a2f3131132dd4c0590223723b
pagoli <71729571+pagoli@users.noreply.github.com>
2023-01-24 12:00:39 +0100

    responsive fixes recipes page

--- src/styles/RecipeStyle.js
+++ src/styles/RecipeStyle.js
@@ -2,19 +2,20 @@ import styled from "styled-components";
 import device from "./device";
 
 const ContentBox = styled.main`
-  /* margin: 2rem 0 0 0; */
   h2 {
     margin: 4rem 0rem;
-    font-size: var(--fz-5xl);
     border-bottom: 1px solid #313131;
   }
 
+  @media ${device.laptop} {
+    h2 {
+      font-size: var(--fz-5xl);
+    }
+  }
   @media ${device.tablet} {
-    /* margin: 1rem 0 0 0; */
     h2 {
       margin: 2rem 0rem;
-      font-size: var(--fz-3xl);
-      border-bottom: 1px solid #313131;
+      font-size: var(--fz-4xl);
     }
   }
 `;
@@ -22,17 +23,9 @@ const ContentBox = styled.main`
 const DetailWrapper = styled.div`
   display: flex;
   flex-direction: row-reverse;
-  .active {
-    background: linear-gradient(35deg, #494949, #313131);
-    color: white;
-  }
-  h3 {
-    font-size: var(--fz-2xl);
-    line-height: 3.5rem;
-    font-weight: normal;
-  }
   a {
-    color: darkred;
+    color: var(--blue);
+
     &:hover {
       text-decoration: underline;
     }
@@ -43,10 +36,9 @@ const DetailWrapper = styled.div`
     padding-left: 3rem;
     li {
       padding-bottom: 1rem;
-      font-size: 2rem;
-      line-height: 2.5rem;
+      font-size: var(--fz-4xl);
       &::marker {
-        color: darkred;
+        color: var(--blue);
       }
     }
   }
@@ -54,45 +46,89 @@ const DetailWrapper = styled.div`
     line-height: 3rem;
   }
 
-  @media ${device.tablet} {
-    flex-direction: column;
-    h3 {
-      font-size: 2rem;
+  @media ${device.laptopL} {
+    h3,
+    ul > li,
+    ol > li {
+      font-size: var(--fz-xl);
       line-height: 3.5rem;
-      font-weight: normal;
+    }
+  }
+
+  @media ${device.laptop} {
+    flex-direction: column;
+    width: 100%;
+    h3 {
+      font-size: var(--fz-xl);
+      line-height: 3rem;
+    }
+  }
+
+  @media ${device.mobileL} {
+    h3,
+    ul > li,
+    ol > li {
+      font-size: var(--fz-md);
+      line-height: 2.2rem;
     }
   }
 `;
 
 const ButtonBox = styled.div`
   display: flex;
-  /* height: 100%; */
-  img {
-    width: 100%;
-  }
+  justify-content: space-evenly;
+  align-items: center;
+  margin: 0 0 3rem;
   @media ${device.tablet} {
     margin: 1rem 0 3rem;
-    /* img {
-      padding: 3rem 0;
-    } */
   }
 `;
 
 const Button = styled.button`
-  font-size: var(--fz-xl);
-  padding: 1rem 2rem;
-  color: #313131;
-  background: white;
-  border: 2px solid black;
+  font-size: var(--fz-4xl);
+  padding: 2rem 4rem;
+  border: 1px solid black;
   border-radius: 10px;
   margin-right: 2rem;
   font-weight: 600;
-  @media ${device.tablet} {
-    font-size: var(--fz-xl);
+  color: var(--white);
+  background: var(--gray);
+  opacity: 70%;
+  &:hover {
+    background: var(--green);
+    color: var(--black);
+    opacity: 100%;
+  }
+  &.active {
+    background: var(--yellow);
+    background: var(--gradient-radial-green-yellow);
+    background-size: 150% 150%;
+    box-shadow: 1px 1px 2px 1px var(--yellow);
+    color: var(--black);
+    opacity: 100%;
+  }
+  &:last-of-type {
+    margin-left: 3rem;
+  }
+  @media ${device.laptopL} {
     margin: 0;
+    font-size: var(--fz-xl);
+    padding: 1rem 2rem;
+  }
+
+  @media ${device.tablet} {
+    font-size: var(--fz-lg);
+    margin: 0;
+  }
+  @media ${device.mobileM} {
+    font-size: var(--fz-sm);
     &:last-of-type {
-      margin-left: 3rem;
+      margin-left: 2rem;
     }
+  }
+
+  @media ${device.mobileS} {
+    padding: 0.8rem 1rem;
   }
 `;
 
@@ -104,16 +140,25 @@ const ImageBox = styled.div`
   img {
     width: 100%;
   }
-  @media ${device.tablet} {
+
+  /* IMAGE SIZE NEEDS TO BE CHECKED */
+  @media ${device.laptop} {
     margin: 1rem 0 3rem;
-    /* img {
-      padding: 3rem 0;
-    } */
+    justify-content: center;
+    /* height: fit-content; */
+    img {
+      /* width: 100%; */
+      height: fit-content;
+    }
   }
 `;
 
 const InfoBox = styled.div`
   width: 80%;
+  margin-bottom: 3rem;
+  @media ${device.laptop} {
+    width: 100%;
+  }
 `;
 
 export { ContentBox, DetailWrapper, ButtonBox, Button, InfoBox, ImageBox };
