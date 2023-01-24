52cd1c22a5537edba9a144e906698595cb1d97f7
pagoli <71729571+pagoli@users.noreply.github.com>
2023-01-24 12:45:16 +0100

    test branch Head

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -2,6 +2,7 @@ import React, { useState, useContext, useEffect } from "react";
 import { Context } from "../context/context";
 import { Wrapper, Grid, Card } from "../styles/CuisineStyle";
 import { Link, useParams } from "react-router-dom";
+//
 
 const Cuisine = () => {
   const [isLoading, setIsLoading] = useState(true);
