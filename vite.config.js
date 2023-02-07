047d82487f2ff3366188157ade1444e2f6f1e5ad
pagoli <71729571+pagoli@users.noreply.github.com>
2023-02-07 08:57:33 +0100

    undid change as recipes not loading

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/easy-recipe-app/",
+  // base: "/easy-recipe-app/",
   // root: "./",
   build: {
     outDir: "dist",
